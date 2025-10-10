53323ff124617f408dcca40120817e51c1fd02b6
DianaArman1216 <dianmap96@icloud.com>
2025-10-09 23:54:26 -0400

    part 2 done

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ from . import (
 urlpatterns = [
     path("", views.home, name="home"),  # root route
     path("about/", views.about, name="about"),  # about route
-    path("cats/", views.cats_index, name="cat-index"),  # index route
+    path("cats/", views.cat_index, name="cat-index"),  # index route
+    path("cats/<int:cat_id>/", views.cat_detail, name="cat-detail"),  # detail route
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
+from .models import Cat
 
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
 
-# Create a list of Cat instances
-cats = [
-    Cat("Lolo", "tabby", "Kinda rude.", 3),
-    Cat("Sachi", "tortoiseshell", "Looks like a turtle.", 0),
-    Cat("Fancy", "bombay", "Happy fluff ball.", 4),
-    Cat("Bonk", "selkirk rex", "Meows loudly.", 6),
-]
+# # Create a list of Cat instances
+# cats = [
+#     Cat("Lolo", "tabby", "Kinda rude.", 3),
+#     Cat("Sachi", "tortoiseshell", "Looks like a turtle.", 0),
+#     Cat("Fancy", "bombay", "Happy fluff ball.", 4),
+#     Cat("Bonk", "selkirk rex", "Meows loudly.", 6),
+# ]
 
 
 # Define the home view function
@@ -30,5 +31,11 @@ def about(request):
     )  # DTL Django Template Language. this checks for a file named about.html in the templates folder
 
 
-def cats_index(request):
-    return render(request, "cats/index.html", {"cats": cats})  # context dictionary
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, "cats/index.html", {"cats": cats})
+
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, "cats/detail.html", {"cat": cat})
